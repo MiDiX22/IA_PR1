7ae674eafb4db89c3629f8c84117ae436bd90f89
miriam <miriam.gertrudix@estudiants.urv.cat>
2021-10-24 18:52:44 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,7 @@ public class Main {
         Solver sv = new Solver(new File("src/Map1.txt"),10, 10,0,0,9,9);
 //        Solver sv = new Solver(new File("src/mapBasic.txt"),3, 3,0,0,2,2);
 
-        System.out.println(sv.getChar(new Point( 0,9)));
-        sv.lookArround(new Node('A',new Point(0,9)));
+        sv.bestFirst();
 
         System.out.println("\n");
         for (int i=0; i<10; i++){
